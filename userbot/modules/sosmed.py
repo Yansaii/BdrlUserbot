c21bba8fd3fff7aa7cf51160c061cfbbf9456382
Bdrl <93611653+Yansaii@users.noreply.github.com>
2022-02-13 02:48:10 +0700

    Update sosmed.py

--- userbot/modules/sosmed.py
+++ userbot/modules/sosmed.py
@@ -11,10 +11,10 @@ from telethon.tl.functions.messages import DeleteHistoryRequest
 
 from userbot import CMD_HANDLER as cmd
 from userbot import CMD_HELP
-from userbot.utils import edit_delete, edit_or_reply, cilik_cmd
+from userbot.utils import edit_delete, edit_or_reply, bdrl_cmd
 
 
-@cilik_cmd(pattern="sosmed(?: |$)(.*)")
+@bdrl_cmd(pattern="sosmed(?: |$)(.*)")
 async def insta(event):
     xxnx = event.pattern_match.group(1)
     if xxnx:
@@ -52,7 +52,7 @@ async def insta(event):
             await xx.delete()
 
 
-@cilik_cmd(pattern="dez(?: |$)(.*)")
+@bdrl_cmd(pattern="dez(?: |$)(.*)")
 async def DeezLoader(event):
     if event.fwd_from:
         return
