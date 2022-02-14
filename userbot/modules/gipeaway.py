16d443a7e3b2623d10157135063541dd8471a033
Bdrl <93611653+Yansaii@users.noreply.github.com>
2022-02-14 17:47:01 +0700

    Update gipeaway.py

--- userbot/modules/gipeaway.py
+++ userbot/modules/gipeaway.py
@@ -1,117 +1,119 @@
 # @greyyvbss
 #cilik-userbot
+# Recode by @BukanBdrl
+
 
 from userbot import CMD_HANDLER as cmd
 from userbot import CMD_HELP
-from userbot.utils import edit_or_reply, cilik_cmd
+from userbot.utils import edit_or_reply, bdrl_cmd
 
 
-@cilik_cmd(pattern="a(?: |$)(.*)")
+@bdrl_cmd(pattern="a(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**ANJAY ADA GIPEAWAY NIH**")
 
 
-@cilik_cmd(pattern="b(?: |$)(.*)")
+@bdrl_cmd(pattern="b(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**BABI SOALNYA GA DANTA BET**")
 
 
-@cilik_cmd(pattern="c(?: |$)(.*)")
+@bdrl_cmd(pattern="c(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**ANJING LAMA BET, TURUNIN SOALNYA**")
 
 
-@cilik_cmd(pattern="d(?: |$)(.*)")
+@bdrl_cmd(pattern="d(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**MANA SOALNYA BUJED, GUA NUNGGUIN DARI TADI**")
 
 
-@cilik_cmd(pattern="e(?: |$)(.*)")
+@bdrl_cmd(pattern="e(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**MINIMAL KALO HADIAHNYA DIKIT, SOALNYA DANTA DIKIT YA ANJENG 😁!!**")
 
 
-@cilik_cmd(pattern="f(?: |$)(.*)")
+@bdrl_cmd(pattern="f(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**HETT GIPEAWAY NYA DIKIT BET, DASAR GC MISKIN CUIHHH!!**")
 
 
-@cilik_cmd(pattern="g(?: |$)(.*)")
+@bdrl_cmd(pattern="g(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**KALO GIPEAWAY SOALNYA YANG BENER YA NGENTOTTT**")
 
 
-@cilik_cmd(pattern="h(?: |$)(.*)")
+@bdrl_cmd(pattern="h(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**MINGGIR-MINGGIR DONATUR MAU LEWAT**")
 
 
-@cilik_cmd(pattern="i(?: |$)(.*)")
+@bdrl_cmd(pattern="i(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**MISKIN MISKIN AJA TOD, GAUSAH SOK SOK AN JADI DONATUR!!**")
 
 
-@cilik_cmd(pattern="j(?: |$)(.*)")
+@bdrl_cmd(pattern="j(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**GCAST DULU AHHHHH, KALI AJA MENANG GIPEAWAY!!**")
 
 
-@cilik_cmd(pattern="k(?: |$)(.*)")
+@bdrl_cmd(pattern="k(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**JAWABAN YANG BENER YANG MANA ANJENG**")
 
 
-@cilik_cmd(pattern="m(?: |$)(.*)")
+@bdrl_cmd(pattern="m(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**PAANSI SOALNYA PRIK BANGET!!**")
 
 
-@cilik_cmd(pattern="n(?: |$)(.*)")
+@bdrl_cmd(pattern="n(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**NAJISSS GIPEAWAY NYA CUMAN CEBAN YANG BANYAKAN NAPAH!!**")
 
 
-@cilik_cmd(pattern="o(?: |$)(.*)")
+@bdrl_cmd(pattern="o(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**OMEGAT GUA MENANG GIPEAWAY, MAYAN BUAT PICIES!!** 🥵🥵")
 
 
-@cilik_cmd(pattern="r(?: |$)(.*)")
+@bdrl_cmd(pattern="r(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**RAME JUGA DISINI, IKUTAN AHHH!!**")
 
 
-@cilik_cmd(pattern="s(?: |$)(.*)")
+@bdrl_cmd(pattern="s(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**GC ANJING SOALNYA GA DANTA SEMUA DAHLAH!!**")
 
 
-@cilik_cmd(pattern="t(?: |$)(.*)")
+@bdrl_cmd(pattern="t(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**HETTT SOALNYA GAMPANG BET, BARI MEREM GE BISA INI MAH** 🥱")
 
 
-@cilik_cmd(pattern="u(?: |$)(.*)")
+@bdrl_cmd(pattern="u(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**UDAH UDAH GIPEAWAY NYA UDAHAN, YAH KASIAN BET KETINGGALAN**")
 
 
-@cilik_cmd(pattern="v(?: |$)(.*)")
+@bdrl_cmd(pattern="v(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**CAPE CAPE GIKES SOALNYA GA DANTA KONTOLL**")
 
 
-@cilik_cmd(pattern="w(?: |$)(.*)")
+@bdrl_cmd(pattern="w(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**CLUE NYA APA ANJENG, SOALNYA GAJELAS**")
 
     
-@cilik_cmd(pattern="x(?: |$)(.*)")
+@bdrl_cmd(pattern="x(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**DIMANA ADA GIPEAWAY DISITU ADA GUA WHAHAHAHAHA**")
     
    
-@cilik_cmd(pattern="z(?: |$)(.*)")
+@bdrl_cmd(pattern="z(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**GUA MAU NURUNIN SOAL NIH , BENTAR MIKIR DULU**")  
     
