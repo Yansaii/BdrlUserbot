e9b6cada20589e97ed1e66b224cb260b9e327bf3
Bdrl <93611653+Yansaii@users.noreply.github.com>
2022-02-14 17:32:18 +0700

    Update hentai.py

--- userbot/modules/hentai.py
+++ userbot/modules/hentai.py
@@ -11,10 +11,10 @@ from telethon.tl.functions.contacts import UnblockRequest
 
 from userbot import CMD_HANDLER as cmd
 from userbot import CMD_HELP
-from userbot.utils import edit_or_reply, cilik_cmd
+from userbot.utils import edit_or_reply, bdrl_cmd
 
 
-@cilik_cmd(pattern="hentai(?: |$)(.*)")
+@bdrl_cmd(pattern="hentai(?: |$)(.*)")
 async def _(event):
     if event.fwd_from:
         return
