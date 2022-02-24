2ce1d13aff1233dd8b3df887e0d9122a81eabaab
Bdrl <93611653+Yansaii@users.noreply.github.com>
2022-02-24 11:57:07 +0700

    Update stats.py

--- userbot/modules/stats.py
+++ userbot/modules/stats.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2020 Catuserbot <https://github.com/sandy1709/catuserbot>
 # Ported by @mrismanaziz
 # FROM Man-Userbot
-# Recode by @greyyvbss
+# Recode by @BdrlBukan
 #
 
 """Count the Number of Dialogs you have in your Telegram Account
